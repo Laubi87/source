fe3fbd7293d5a250874ee2d0766fcd5b7e2366a2
Simon Laubscher <simon.laubscher@web.de>
2020-08-07 23:02:05 +0200

    sign up and login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(form_params)
+
+    if @user.save
+
+      # keep hold of that user
+      session[:user_id] = @user.id
+
+      # welcome message
+      flash[:success] = "Welcome to Source!"
+
+      redirect_to root_path
+    else
+      render "new"
+    end
+
+  end
+
+
+
+  def form_params
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+  end
+end
